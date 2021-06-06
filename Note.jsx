0c8404ee15cfae4cd9e3c935ef9fddfc5c4327d0
pra2003 <84983546+pra2003@users.noreply.github.com>
2021-06-05 23:50:04 -0800

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>java script and rect.js HTML</h1>
+      <p>
+        this was amazing bootcamp taken up by Prachi kumawat we convered
+        everything from scratch including javascript React.js, HTML. this is
+        amazing.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
